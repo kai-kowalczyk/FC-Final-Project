5e221f830db1f469c570407f4ed9a114615fcd60
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-11-22 23:25:43 +0100

    created database models

--- it_job_data/data_scraper/models.py
+++ it_job_data/data_scraper/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Offers(models.Model):
+    offer_id = models.CharField(max_length=50)
+    offer_full_link = models.CharField(max_length=200)
+    position_title = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=50)
+    min_salary = models.IntegerField()
+    max_salary = models.IntegerField()
--- main.py
+++ main.py
@@ -4,42 +4,60 @@ import json
 import random
 
 #JUSTJOIN.IT OFFERS
-jjit_offers = requests.get('https://justjoin.it/api/offers').json()
+'''jjit_offers = requests.get('https://justjoin.it/api/offers').json()
 for i in range(0, 49):
-    print(jjit_offers[i])
+    print(jjit_offers[i])'''
 
 #NOFLUFFJOBS.COM OFFERS
-seniority = ['trainee', 'junior', 'mid', 'senior', 'expert']
-page_number = 1
+class NFJOffers:
 
-nfj_html = requests.get(f'https://nofluffjobs.com/pl/praca-it?criteria=seniority%3D{seniority[0]}&page={page_number}').text
+    BASE_URL = 'https://nofluffjobs.com'
 
-nfj_webpage = BeautifulSoup(nfj_html, 'lxml')
-page_number_links = nfj_webpage.find_all(class_='page-link')
-number_of_pages = []
-for i in range(1, len(page_number_links)-1):
-    number_of_pages.append(page_number_links[i].text)
-max_page_number = max(number_of_pages)
-offers = nfj_webpage.find_all('a', class_='posting-list-item')
-for i in range(len(offers)):
-    offer = offers[i]
-    offer_id = offer.get('id')
-    print(offer_id)
-    offer_link = offer.get('href')
-    print(f'https://nofluffjobs.com{offer_link}')
-    position_title = offer.find('h3', class_='posting-title__position').text
-    print(position_title)
-    company_name = offer.find('span', class_='posting-title__company').text
-    print(company_name)
-    salary = offer.find(class_='salary' ).text
-    print(salary)
-    
+    def __init__(self):
+        self.seniority = ['trainee', 'junior', 'mid', 'senior', 'expert']
+        self.page_number = 1
+        self.max_page_number = 0
+
+    def request_nfj_data(self):
+        self.nfj_html = requests.get(f'{self.BASE_URL}/pl/praca-it?criteria=seniority%3D{self.seniority[0]}&page={self.page_number}').text
+        #parse the html
+        self.nfj_webpage = BeautifulSoup(self.nfj_html, 'lxml')
+
+    def everything_else(self):
+#find out how many pages of results there are and pick the highest number
+        page_number_links = self.nfj_webpage.find_all(class_='page-link')
+        number_of_pages = []
+        for i in range(1, len(page_number_links)-1):
+            number_of_pages.append(page_number_links[i].text)
+            self.max_page_number = max(number_of_pages)
+
+#find all the 'offer' items on the page
+        offers = self.nfj_webpage.find_all('a', class_='posting-list-item')
+
+#get all the needed information from the offer
+        for i in range(1,6):
+            offer = offers[i]
+            offer_id = offer.get('id')
+            print(f'#######{offer_id}########## \n')
+            offer_link = offer.get('href')
+            offer_full_link = f'https://nofluffjobs.com{offer_link}'
+            print(f'########{offer_full_link}######## \n')
+            position_title = offer.find('h3', class_='posting-title__position').text
+            print(f'#######{position_title}########## \n')
+            company_name = offer.find('span', class_='posting-title__company').text
+            print(f'#######{company_name}########## \n')
+            salary = offer.find(class_='salary' ).text
+            print(f'#######{salary}########## \n')
+            offer_html = requests.get(offer_full_link).text
+            offer_parser = BeautifulSoup(offer_html, 'lxml')
+            skills = []
+            required = offer_parser.find_all('common-posting-item-tag')
+            for i in range(len(required)):
+                skill = required[i]
+                skills.append(skill.find(class_='btn').text)
+            print(f'#######{skills}########## \n')
 
 
-
-    
-
-#print(offers.prettify())
-#print(nfj_webpage.prettify())
-
-
+nfj = NFJOffers()
+nfj.request_nfj_data()
+nfj.everything_else()
