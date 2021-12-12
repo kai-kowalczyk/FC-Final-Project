4411d08aef9951cbe6f70e1dfbab34c54e203149
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-12-12 13:49:59 +0100

    changed button color, working on styles

--- it_job_data/data_scraper/views.py
+++ it_job_data/data_scraper/views.py
@@ -15,10 +15,12 @@ def index(request):
     return HttpResponse("Hello, world. You're at the data_scraper index.")
 
 def home_page(request):
-    top_offers = Offer.best_paid_jobs(Offer, 'junior')
+    top_junior = Offer.best_paid_jobs(Offer, 'junior')
+    top_senior = Offer.best_paid_jobs(Offer, 'senior')
     index_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    offer_data = dict(zip(index_list, top_offers))
-    return render(request, 'data_scraper/homepage.html', context={'top_offers': offer_data})
+    junior_data = dict(zip(index_list, top_junior))
+    senior_data = dict(zip(index_list, top_senior))
+    return render(request, 'data_scraper/homepage.html', context={'top_junior': junior_data, 'top_senior': senior_data})
 
 def offers_page(request):
     return render(request, 'data_scraper/offers.html')
